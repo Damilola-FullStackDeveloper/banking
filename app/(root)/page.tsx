e2f327848155bb14075608e52492cabfdcbd25f4
Damilola-FullStackDeveloper <bakarewealth@gmail.com>
2024-08-14 12:20:08 +0100

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = {firstName: 'Damilola',   lastName: 'Bakare', email:'bakarewealth@gmail.com'};
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+           <header className="home-header">
+            <HeaderBox
+             type="greeting"
+             title="Welcome"
+             user={loggedIn?.firstName || 'Guest'}
+             subtext="Access and managed your account and transactions efficiently."
+            /> 
+
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={12000000000}
+            />
+           </header>
+           RECENT TRANSACTIONS
+        </div>
+       
+       <RightSidebar
+         user={loggedIn}
+         transactions={[]}
+         banks={[{currentBalance: 567.60}, {currentBalance: 755.53}]}
+       />
+    </section>
   )
 }
 
 export default Home
+
